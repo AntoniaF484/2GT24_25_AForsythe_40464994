23403002a6889a3efc5d643ae788cc01bc24c63b
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-10-06 22:51:06 +0100

    Basic Gameplay - Up to 2.24
    Create with Code 2 - Up to Lesson 2.24

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Create with Code 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Create with Code 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange  =10;
+    public float xRange  =10.0f;
+    public GameObject projectilePrefab;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+    { }
 
     // Update is called once per frame
     void Update()
-
-    {//Keep the player in both bounds
+{
+    //Keep the player in both bounds
         if (transform.position.x< -xRange)
         {
             transform.position= new Vector3(-xRange,transform.position.y,transform.position.z);
@@ -28,7 +30,15 @@ public class PlayerController : MonoBehaviour
             transform.position= new Vector3(xRange,transform.position.y,transform.position.z);   
          }
 
-        horizontalInput = Input.GetAxis("Horizontal");
+     horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
-    }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        //Launch a projectile from the player
+        {
+            Instantiate (projectilePrefab,transform.position,projectilePrefab.transform.rotation);
+            }
+}
+
+    
 }
