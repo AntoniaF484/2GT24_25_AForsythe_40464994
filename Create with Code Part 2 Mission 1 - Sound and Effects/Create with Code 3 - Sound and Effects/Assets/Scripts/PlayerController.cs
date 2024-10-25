9c85681bfc2158b118cd9edddd1530e00ca63814
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-10-25 20:18:35 +0100

    Lesson 3.3 completed
    Create with Code 3 - Lesson 3.3 completed

--- Create with Code Part 2 Mission 1 - Sound and Effects/Create with Code 3 - Sound and Effects/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 2 Mission 1 - Sound and Effects/Create with Code 3 - Sound and Effects/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
 
     public float gravityModifier;
     public bool isOnGround = true;
+public bool gameOver;
+private Animator playerAnim;
 
 
     // Start is called before the first frame update
@@ -17,22 +19,32 @@ public class PlayerController : MonoBehaviour
     {
         playerRB = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-    }
+playerAnim=GetComponent<Animator>();
+    
+}
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver==false)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+playerAnim.SetTrigger("Jump_trig");
         }
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+if (collision.gameObject.CompareTag("Ground")){
+ isOnGround = true;
+} else if (collision.gameObject.CompareTag("Obstacle")){
+gameOver=true;
+Debug.Log("Game Over!");
+playerAnim.SetBool("Death_b",true);
+playerAnim.SetInteger("DeathType_int",1);}
+
 
     }
 }
