7e37ae42e86a068c3031169e7bb192132cfd2b98
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-11-10 16:40:53 +0000

    Create with Code 5 - 5.2 Completed

--- Create with Code Part 2 Mission 3- User Interface/Create with Code 5  - User Interface/Assets/Scripts/Target.cs
+++ Create with Code Part 2 Mission 3- User Interface/Create with Code 5  - User Interface/Assets/Scripts/Target.cs
@@ -13,13 +13,22 @@ public class Target : MonoBehaviour
     [SerializeField]private float maxTorque = 10;
     [SerializeField] private float xRange = 4;
     [SerializeField] private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
-        targetRb = GetComponent<Rigidbody>();
+        targetRb = GetComponent<Rigidbody>(); 
+        
        targetRb.AddForce(RandomForce(),ForceMode.Impulse);
        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
        transform.position = RandomSpawnPos();
+       
+       gameManager = GameObject.Find("GameManager").GetComponent<GameManager> ();
              } 
 
     // Update is called once per frame
@@ -31,6 +40,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
