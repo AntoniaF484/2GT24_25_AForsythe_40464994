064323abbcbf4b9f57d06187992ceea30e824a14
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-12-01 20:52:53 +0000

    Create with code 6.1 - Activities on Prototype 1

--- Create with Code Part 1 Mission 1 - Player Control/Create with Code 1 -Player Control/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Create with Code 1 -Player Control/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
-    private float speed = 20.0f;
-    private float turnSpeed = 25.0f;
+    [SerializeField] float speed = 20.0f;
+    [SerializeField] float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+  
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
